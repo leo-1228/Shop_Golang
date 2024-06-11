7fe725b37e47fd19fbbbbdaf18bd7c2876fd044a
leo-1228 <akiraito731@gmail.com>
2024-06-11 01:23:18 -0400

    complete http server

---
+++ internal/server/http/server.go
@@ -0,0 +1,76 @@
+package http
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quangdangfit/gocommon/logger"
+	"github.com/quangdangfit/gocommon/validation"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "goshop/docs"
+	orderHttp "goshop/internal/order/port/http"
+	productHttp "goshop/internal/product/port/http"
+	userHttp "goshop/internal/user/port/http"
+	"goshop/pkg/config"
+	"goshop/pkg/dbs"
+	"goshop/pkg/redis"
+	"goshop/pkg/response"
+)
+
+type Server struct {
+	engine    *gin.Engine
+	cfg       *config.Schema
+	validator validation.Validation
+	db        dbs.IDatabase
+	cache     redis.IRedis
+}
+
+func NewServer(validator validation.Validation, db dbs.IDatabase, cache redis.IRedis) *Server {
+	return &Server{
+		engine:    gin.Default(),
+		cfg:       config.GetConfig(),
+		validator: validator,
+		db:        db,
+		cache:     cache,
+	}
+}
+
+func (s Server) Run() error {
+	_ = s.engine.SetTrustedProxies(nil)
+	if s.cfg.Environment == config.ProductionEnv {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	if err := s.MapRoutes(); err != nil {
+		log.Fatalf("MapRoutes Error: %v", err)
+	}
+	s.engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	s.engine.GET("/health", func(c *gin.Context) {
+		response.JSON(c, http.StatusOK, nil)
+		return
+	})
+
+	// Start http server
+	logger.Info("HTTP server is listening on PORT: ", s.cfg.HttpPort)
+	if err := s.engine.Run(fmt.Sprintf(":%d", s.cfg.HttpPort)); err != nil {
+		log.Fatalf("Running HTTP server: %v", err)
+	}
+
+	return nil
+}
+
+func (s Server) GetEngine() *gin.Engine {
+	return s.engine
+}
+
+func (s Server) MapRoutes() error {
+	v1 := s.engine.Group("/api/v1")
+	userHttp.Routes(v1, s.db, s.validator)
+	productHttp.Routes(v1, s.db, s.validator, s.cache)
+	orderHttp.Routes(v1, s.db, s.validator)
+	return nil
+}
